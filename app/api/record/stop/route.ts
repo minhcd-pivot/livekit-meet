63ed050debbf1761067f9b6c76d5e16cb33d1858
minhcd-pivot <minhcd@pivot.vn>
2025-07-16 16:29:12 +0700

    disable https

--- app/api/record/stop/route.ts
+++ app/api/record/stop/route.ts
@@ -19,7 +19,7 @@ export async function GET(req: NextRequest) {
     const { LIVEKIT_API_KEY, LIVEKIT_API_SECRET, LIVEKIT_URL } = process.env;
 
     const hostURL = new URL(LIVEKIT_URL!);
-    hostURL.protocol = 'https:';
+    // hostURL.protocol = 'https:';
 
     const egressClient = new EgressClient(hostURL.origin, LIVEKIT_API_KEY, LIVEKIT_API_SECRET);
     const activeEgresses = (await egressClient.listEgress({ roomName })).filter(
